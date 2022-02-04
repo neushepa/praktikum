c4f7dcd2f364e725cb7891a98772211f33470523
Rio Andrianto <r.andrianto@gmail.com>
2022-02-04 13:17:06 +0700

    Add Simple Login Logout

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -40,11 +40,11 @@
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
               </li>
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+                <div>Hi, {{ Auth::user()->name }}</div> <a button href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-danger">Logout</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+             </div>
         </div>
       </nav>
     <center>{{ $greetings }}</center>
